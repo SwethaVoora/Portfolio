e4d318536abb1afbf228bbffe081ad7fa66e0386
SwethaVoora <swetha.voora01@gmail.com>
2024-05-02 14:25:12 -0400

    Basic Portfolio

---
+++ components/about.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-8">
+        After completing my Bachelors from one of{" "}
+        <span className="font-semibold">
+          top National Universities in India
+        </span>
+        , I have always been excited to work on the projects that were assigned
+        to me. Seeing things go from the stage of Design to Deployment and
+        letting the code do its magic, makes me feel accomplished and encourage
+        me to do more. After working as a{" "}
+        <span className="font-semibold">
+          Software Engineer for more than 4 years
+        </span>
+        , I decided to follow my passion for software development further by
+        pursuing{" "}
+        <span className="font-semibold">MSCS at Pace University, New York</span>
+        . Over the years of my experience as a Software Engineer, I believe the
+        companies I worked with never fell short of nurturing my
+        cross-communication/technical skills. Im confident that my nature of
+        being accountable for my tasks, ability to learn and adapt well in
+        dynamic/fast paced work environments and the thirst to explore new
+        technologies make me a good fit in any team that welcomes me. Im easy to
+        communicate with, a great team player and would always prefer a team
+        setting where I can develop, mentor and learn alongisde my
+        colleagues/peers.
+        {/* Leetcode(about 150 problems), ability to learn and adapt well at dynamic work environments, Work experience has helped me nurture my cross-communication skills,
+        Looking forward to leverage my skills in any place welcoming me. */}
+      </p>
+      {/* <p className="mb-3">
+        After graduating with a degree in{" "}
+        <span className="font-medium">Accounting</span>, I decided to pursue my
+        passion for programming. I enrolled in a coding bootcamp and learned{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          React, Next.js, Node.js, and MongoDB
+        </span>
+        . I am also familiar with TypeScript and Prisma. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+      </p> */}
+
+      <p>
+        As a soon-to-be graduate in May 2024, I'm actively looking for full-time
+        opportunities in{" "}
+        <span className="font-semibold">
+          Software Engineer/Frontend Engineer/Full stack Engineer roles.
+        </span>
+      </p>
+      {/* <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">history and philosophy</span>. I'm also
+        learning how to play the guitar.
+      </p> */}
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,148 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import restImg from "@/public/FeastRest.png";
+import staywellImg from "@/public/staywell.png";
+import amazonProjectImg from "@/public/AmazonProject.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "National Institute of Technology, Rourkela, India",
+    location: "B.Tech in Computer Science & Engineering",
+    description:
+      "I set my foot in the world of Computer Science at NIT Rourkela. This Institute has taught me beyond what I could ever imagine, from the basics of DSA to the depths of Deep Learning.",
+    icon: React.createElement(LuGraduationCap),
+    date: "Aug 2014 - May 2018",
+  },
+  {
+    title: "Associate Software Engineer",
+    location: "Parexel International",
+    description:
+      "Right after my Bachelors, My first full-time job was at Parexel International, where I constantly worked with global teams in US/UK and was actively working on Change Requests which were assigned to me via JIRA Software.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Jun 2018 - Dec 2020",
+  },
+  {
+    title: "Software Engineer",
+    location: "Calyx",
+    description:
+      "I would say that my hands were always full during my tenure at Calyx, from CRs to end-to-end full stack projects, documentations, databases and more. Overall a fast paced work environment which never left me bored.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Jan 2021 - Jul 2022",
+  },
+  {
+    title:
+      "Pace University - Seidenberg school of Computer Science and Information Systems",
+    location: "Master of Science, Computer Science",
+    description:
+      "Pursuing MSCS at Pace has been incredibly rewarding, providing me with the opportunity to deepen my skills while focusing on academic/personal projects, LeetCode challenges, and building connections across diverse backgrounds.",
+    icon: React.createElement(LuGraduationCap),
+    date: "Sep 2022 - May 2024",
+  },
+  {
+    title: "Full stack development Intern",
+    location: "Shoptaki",
+    description:
+      "In Fall 2023, I worked as a Intern at Shoptaki and was leading the full stack development of their crowdfunding real-estate platform, Sophtera.",
+    // I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "Sep 2023 - Dec 2023",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "FeastRest",
+    description:
+      "FeastRest is a full stack food-order management system for dine-in customers.",
+    tags: [
+      "React",
+      "Next.js",
+      "TailwindCSS",
+      "Auth0",
+      "AWS Services",
+      "RDS",
+      "API Gateway",
+      "Lambda",
+    ],
+    imageUrl: restImg,
+  },
+  {
+    title: "Staywell",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: [
+      "React",
+      "TailwindCSS",
+      "Redux",
+      "Sequelize",
+      "Express.js",
+      "Jenkins",
+    ],
+    imageUrl: staywellImg,
+  },
+  {
+    title: "Amazon Clone",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["Javascript", "HTML", "CSS"],
+    imageUrl: amazonProjectImg,
+  },
+] as const;
+
+export const skillsData = [
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Express",
+  "HTML",
+  "CSS",
+  "Tailwind",
+  "Java",
+  "Spring Boot",
+  "C#",
+  "ASP.NET",
+  "Jenkins",
+  "Docker",
+  "CI/CD",
+  "Git",
+  "Sequelize",
+  "MySQL",
+  "PostgreSQL",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "Python",
+  "Django",
+  "Flask",
+] as const;
